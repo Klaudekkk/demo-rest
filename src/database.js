b7bb2c29a31c3d85fa8dcfdedbd179735655efaf
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-01-22 19:52:50 +0100

    second commit

--- src/database.js
+++ src/database.js
@@ -1,51 +1,102 @@
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 
 class Database {
-  data = [];
-  menu = [];
-
-  constructor() {
-    this.data.push({
-      id: uuidv4(),
-      description: "Example",
-      createDate: new Date(),
-      status: "open",
-    });
-
-    this.menu.push({
-      id: uuidv4(),
-      name: "Example",
-      price: 24.99,
-    });
-  }
+    data = [];
+    menu = [];
+    order = [];
 
 
-  getAll() {
-    return this.data;
-  }
+    constructor() {
+        this.data.push({
+            id: uuidv4(),
+            description: "Example",
+            createDate: new Date(),
+            status: "open",
+        });
+    }
 
-  getById(id) {
-    return this.data.find((item) => item.id === id);
-  }
+    getAll() {
+        return this.data;
+    }
 
-  getByStatus(status) {
-    return this.data.find((item) => item.status === status);
-  }
+    getModelMenu({name, products, uuid}) {
+        const modelMenu = {
+            id: uuid,
+            name: name,
+            createDate: new Date(),
+            products: this.getModelProducts(products),
+        };
+        return modelMenu;
+    }
 
-  create(description) {
-    const item = {
-      id: uuidv4(),
-      description,
-      createDate: new Date(),
-      status: "open",
-    };
+    createMenu({name, products, uuid = uuidv4()}) {
+        const menu = this.getModelMenu({name, products, uuid});
+        this.menu.push(menu);
+        return menu;
+    }
 
+    addOrder(order) {
+        const modelOrder = this.getModelOrder(order);
+        this.order.push(modelOrder);
+        return modelOrder;
+    }
 
-    this.data.push(item);
+    getModelOrder({productsIds, tableNumber, menuId}) {
+        const order ={
+            id: uuidv4(),
+            productsIds: productsIds,
+            menuId: menuId,
+            creationDate: new Date(),
+            tableNumber: tableNumber
+        }
+        return order;
+    }
 
-    return item;
-  }
+    getOrderById(orderId) {
+        for(const order of this.order) {
+            if (orderId === order.id){
+                return order
+            }
+        }
+    }
 
+    getOrder(orderId) {
+        const {productsIds, menuId} = this.getOrderById(orderId)
+        const products = [];
+        productsIds.forEach(productId => {
+            products.push(
+                this.getProductFromMenu(menuId, productId)
+            )
+        })
+        return products;
+    }
+
+    getProductFromMenu(menuId, productId) {
+        const menu = this.getMenuById(menuId);
+        for (const menuProduct of menu.products) {
+            if (productId === menuProduct.id ){
+                return menuProduct
+            }
+        }
+    }
+
+    getModelProducts(products) {
+        const modelProducts = [];
+        for (const product of products) {
+            modelProducts.push(this.getModelProduct(product))
+        }
+        return modelProducts
+    }
+
+    getModelProduct({name, price, uuid = uuidv4()}) {
+        const product = {
+            id: uuid,
+            name: name,
+            createDate: new Date(),
+            price: price
+        };
+        return product
+    }
 
   create(product) {
     const item = {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,56 +1,73 @@
 import database from "../database.js";
+import {getOrder, postOrder} from "./orderRoutes.js";
+import {getMenu, updateMenu, postMenu } from "./menuRoutes.js";
+
 
 export default [
-  {
-    method: "GET",
-    path: "/",
-    cbs: [
-      (req, res, next) => {
-        console.log(`New request [${new Date()}] ${JSON.stringify(req.body)}`);
-        next();
-      },
-      (req, res) => {
-        res.json(database.getAll());
-      },
-    ],
-  },
-  {
-    method: "POST",
-    path: "/",
-    cbs: [
-      (req, res) => {
-        const { body } = req;
-        const { description } = body;
-        const result = database.create(description);
-        res.json(result);
-      },
-    ],
-  },
-  {
-    method: "GET",
-    path: "/menu",
-    cbs: [
-      (req, res, next) => {
-        console.log(`New request [${new Date()}] ${JSON.stringify(req.body)}`);
-        next();
-      },
-      (req, res) => {
-        const result = database.getMenu();
-        res.json(result);
-      },
-    ],
-  },
-  {
-    method: "POST",
-    path: "/menu",
-    cbs: [
-      (req, res) => {
-        const { body } = req;
-        const {id, name, price } = body;
-        database.AddtoMenu(id,name,price);
-        res.status(201);
-        res.json({name, status});
-      },
-    ],
-  },
+    {
+        method: "GET",
+        path: "/",
+        cbs: [
+            (req, res, next) => {
+                console.log(`New request [${new Date()}] ${JSON.stringify(req.body)}`);
+                next();
+            },
+            (req, res) => {
+                res.json(database.getAll());
+            },
+        ],
+    },
+    {
+        method: "POST",
+        path: "/",
+        cbs: [
+            (req, res) => {
+                const {body} = req;
+                const {description} = body;
+                const result = database.create(description);
+                res.json(result);
+            },
+        ],
+    },
+    {
+        method: "GET",
+        path: "/menu",
+        cbs: [
+            (req, res, next) => {
+                console.log(`New request [${new Date()}] ${JSON.stringify(req.body)}`);
+                next();
+            },
+            getMenu,
+        ],
+    },
+    {
+        method: "POST",
+        path: "/menu",
+        cbs: [
+            postMenu,
+        ],
+    },
+    {
+        method: "PUT",
+        path: "/menu",
+        cbs: [
+            updateMenu,
+        ],
+    },
+    {
+        method: "GET",
+        path: "/order",
+        cbs: [
+            getOrder,
+        ],
+    },
+    {
+        method: "POST",
+        path: "/order",
+        cbs: [
+            postOrder,
+        ],
+    },
+
+
 ];
